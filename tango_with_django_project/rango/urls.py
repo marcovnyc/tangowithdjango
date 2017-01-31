5a384da88f30db8cd3548d271f6ecdbd6316cf32
marcovnyc <marcovnyc@gmail.com>
2017-01-30 23:24:34 -0500

    Added the fixed to about page is work now

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,5 +3,5 @@ from rango import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^$', views., name='about'),
+        url(r'^about', views.about, name='about'),
 )
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def about(request):
-    return HttpResponse("Rango says there world!")
+def index(request):
+    return HttpResponse("Welcome to the Matrix!")
 
 def about(request):
-    return HttpResponse("About this page")
+    return HttpResponse("The Matrix is a fake reality, do you want to take the blue or redpill?")
